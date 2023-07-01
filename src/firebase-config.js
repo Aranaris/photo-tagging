bb1789114c79960269c77a1d650206604d823303
vinceye <vinceye.cm@gmail.com>
2023-07-01 16:53:31 -0700

    re-adding firebase-config

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADiwMtW8co4O8qA5nY28-pmnBxGo04o_o",
+    authDomain: "photo-tagging-70c04.firebaseapp.com",
+    databaseURL: "https://photo-tagging-70c04-default-rtdb.firebaseio.com",
+    projectId: "photo-tagging-70c04",
+    storageBucket: "photo-tagging-70c04.appspot.com",
+    messagingSenderId: "90440877471",
+    appId: "1:90440877471:web:0cba0d6376bc89ab7f69aa"
+  };
+
+export default firebaseConfig;
