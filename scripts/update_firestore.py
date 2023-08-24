97b72c2376e0e7edbdb9bb470cdb504cf89401c4
vinceye <vinceye.cm@gmail.com>
2023-08-24 14:33:52 -0700

    parse json file and update image data in firestore with tags

--- scripts/update_firestore.py
+++ scripts/update_firestore.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
 
 cred = credentials.Certificate('./photo-tagging-70c04-c47de2960785.json')
 
@@ -8,4 +9,8 @@ app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-print(cred, app, db)
+with open("sample.json", "r") as infile:
+    data = json.load(infile)
+    for image in data['images']:
+        image_id = image['image']
+        db.collection('images').document(image_id).set(image)
